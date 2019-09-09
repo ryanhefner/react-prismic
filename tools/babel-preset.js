8398e80940f69141a9c98d39ea6c209f67028335
Ryan Hefner <hi@ryanhefner.com>
2019-09-09 15:31:51 -0400

    Enable Babel plugin

--- tools/babel-preset.js
+++ tools/babel-preset.js
@@ -9,7 +9,7 @@ const plugins = [
 
 if (process.env.NODE_ENV === 'production') {
   plugins.push(
-    // 'babel-plugin-dev-expression',
+    'babel-plugin-dev-expression',
     'babel-plugin-transform-react-remove-prop-types'
   );
 }
